511bcb50f4985c435b6f4ba8c6cca6fd5d65a50f
Gildeir <gildeirlopes@gmail.com>
2021-04-28 21:56:35 -0300

    Adicionando title, subtitle, storyLine ao movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,21 +5,14 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-
+    // const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { imagePath } = movie;
     return (
       <div>
-        Nome:
-        {movie.title}
-        {/*       ,
-        {movie.title}
-        ,
-        {movie.storyline}
-        ,
-        {movie.rating}
-        ,
-        {movie.imagePath}
-        ,
-        <img src={ movie.imagePath } alt={ movie.title } /> */}
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <img src={ imagePath } alt={ movie.title } />
       </div>
     );
   }
